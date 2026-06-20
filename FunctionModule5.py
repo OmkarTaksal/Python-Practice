4a841e7aafc0bca1fa52be1f9dffcd1c227001fc
Omkar Taksal <omkartaksal12@gmail.com>
2026-06-20 23:14:56 +0530

    Python Program

---
+++ FunctionModule5.py
@@ -0,0 +1,20 @@
+from Marvellous import *
+
+def main():
+    print("Enter First Number")
+    Value1=int(input())
+    
+    print("Enter Second Number")
+    Value2=int(input())
+    
+    Ret=Addition(Value1,Value2)  
+    print("Addition is :",Ret)
+    
+    Ret=Subtraction(Value1,Value2) 
+    print("Subtraction is :",Ret)
+    
+if __name__=="__main__":
+    main()
+    
+    
+    
---
+++ StarterMain.py
@@ -0,0 +1,6 @@
+def main():
+    pass  #Empty Function
+    
+if __name__=="__main__":
+    main()
+    
